9bf42cc5bdfaa4f4b6f34771a30ff29c74684a8a
Lorenzo Vicino <imlorenzovicino@gmail.com>
2025-04-06 20:55:15 +0200

    feat: :sparkles: first commit of my report engine spring application

---
+++ src/main/java/com/github/lorenzovicino/report_engine/ReportEngineApplication.java
@@ -0,0 +1,13 @@
+package com.github.lorenzovicino.report_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lorenzovicino/report_engine/ReportEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lorenzovicino.report_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
